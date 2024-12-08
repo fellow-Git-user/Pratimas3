93d4dbe4678452f08f8436b3ab193d6d91407eae
fellow-Git-user <martynas.nadeika@gmail.com>
2024-12-08 17:06:07 +0200

    exercise with tenary done

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,18 @@
 // } else {
 //     alert( 'Rimtai? Taigi Vatikanas!' );
 // } 
+
+// let a = 1;
+// let b = 3;
+
+// let result = (a + b < 4) ? 'below' : 'over';
+// console.log(result);
+
+// let login = prompt('who is logging in', 'Your work position');
+
+// let message = (login == 'Employee') ? 'Hello' :
+// (login == 'Director') ? 'Greetings' :
+// (login == '') ? 'No login' :
+// 'Get out';
+
+// alert( message);
