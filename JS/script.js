a0e485c64d1fa8022382bb3744429e657791a190
fellow-Git-user <martynas.nadeika@gmail.com>
2024-12-08 16:51:41 +0200

    first exercise

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// let smallestCountry = prompt('Kokia yra maziausia salis pasaulyje?', '');
+
+// if (smallestCountry == 'Vatikanas'){
+//     alert( 'Teisingai!' );
+// } else {
+//     alert( 'Rimtai? Taigi Vatikanas!' );
+// } 
